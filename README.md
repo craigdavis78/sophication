f54765a4e55be8890274698ad2c6b7417f14585e
Craig Davis <craigdavis78@gmail.com>
2019-11-15 04:47:46 -0600

    Added several packaging realated fixes

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # sophication
-A multiplication flash card program for my daughter, Sophia
+A command-line based flash card program written in python.  It is currently
+a VERY rudimentary application; only handling multiplication flash cards.<br>
+<br>
+The sophication package is my sandbox project to learn packaging and typing for
+python. I will add additional functionality after I have packaged and type the
+simple one.  The application currently has a text-to-speech component, but I am
+unsure if that will remain.<br>
+<br>
+## URL
+[sophication](https://github.com/craigdavis78/sophication)<br>
+<br>
+## Example Usage<br>
+This will generate a random set of flash cards for multiplication of 3's and 4's.<br>
+```
+$ python sophication.py --name=Bob 3 4
+```
--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
+"""The setup script for the sophication package."""
 import setuptools
 
 with open("README.md", "r") as file_reader:
     long_description = file_reader.read()
 
 setuptools.setup(
-
+    name="sophication",
+    version="0.0.1",
+    author="Craig Davis",
+    author_email="craigdavis78@gmail.com",
+    description="A simple flash card program",
+    long_description=long_description,
+    log_description_content="text/markdown",
+    url="https://github.com/craigdavis78/sophication",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Education",
+        "Typing :: Typed"
+    ],
+    python_requires='>=3.6'
 )
